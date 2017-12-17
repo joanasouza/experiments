888152a28463ce4cad8551afcae9c4e7a8e45853
Joana Souza <souzajoana18@gmail.com>
2017-12-17 16:31:36 -0300

    generic postfixcalc function

--- tdd/postfix-calculator/calculator.py
+++ tdd/postfix-calculator/calculator.py
@@ -17,13 +17,16 @@ def postfix_calc(expression):
     if len(expr_list) == 1:
         return int(expr_list[0])
     elif len(expr_list) == 3:
+        if expr_list[2] != '+':
+            raise ValueError('Unknown operator: %s' % expr_list[2])
         return int(expr_list[0]) + int(expr_list[1])
     else:
-        if expr_list[3].isdigit():
-            result = postfix_calc(expr_list[0:3])  
-            return postfix_calc([result, expr_list[3], expr_list[4]])
-        else:
-            result = postfix_calc(expr_list[1:4])  
-            return postfix_calc([expr_list[0], result, expr_list[4]])
+        pos = expr_list.index('+')
+        result = postfix_calc(expr_list[pos-2:pos+1])  
+        new_expr_list = expr_list[0:pos-2]
+        new_expr_list.append(result)
+        new_expr_list.extend(expr_list[pos+1:])
+
+        return postfix_calc(new_expr_list)
 
 
