fc4f23be3a1f71f103447eab6b12abf509b76230
Joana Souza <souzajoana18@gmail.com>
2017-12-17 16:32:06 -0300

    add complex_sum test function

--- tdd/postfix-calculator/tests/test_calculator.py
+++ tdd/postfix-calculator/tests/test_calculator.py
@@ -2,16 +2,22 @@ import unittest
 from calculator import postfix_calc  
 
 class CalculatorTest(unittest.TestCase):
-	
-	def test_simple_expression(self):
-		self.assertEqual(1, postfix_calc('1'))
-		self.assertEqual(2, postfix_calc('2'))
-		self.assertEqual(3, postfix_calc('3'))
-		self.assertEqual(4, postfix_calc('4'))
+
+    def test_simple_expression(self):
+        self.assertEqual(1, postfix_calc('1'))
+        self.assertEqual(2, postfix_calc('2'))
+        self.assertEqual(3, postfix_calc('3'))
+        self.assertEqual(4, postfix_calc('4'))
 
 
-	def test_simple_sum(self):
-		self.assertEqual(2, postfix_calc('1 1 +'))
-		self.assertEqual(3, postfix_calc('1 2 +'))
-		self.assertEqual(9, postfix_calc('2 2 5 + +'))
-		self.assertEqual(9, postfix_calc('2 2 + 5 +'))
+    def test_simple_sum(self):
+        self.assertEqual(2, postfix_calc('1 1 +'))
+        self.assertEqual(3, postfix_calc('1 2 +'))
+        self.assertEqual(9, postfix_calc('2 2 5 + +'))
+        self.assertEqual(9, postfix_calc('2 2 + 5 +'))
+
+    def test_complex_sum(self):
+        self.assertEqual(8, postfix_calc('1 1 1 + 2 3 + + +'))
+
+       # 1 2 2
+       # add more tests to complex sum
